d44019aca58e37ed4170cf16960abcad1c51b63a
Stefan Boesen <stefan.boesen@gmail.com>
2012-07-26 11:53:02 -0400

    Added base rails files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LdapDartmouth::Application.config.secret_token = 'e08f630444b191e7d362ab36293913c49b338a59466d3843fef1da5b4eccc396b4ada89824fa24f83f989cc97d4455b31d30bd714c7752ffd4a2115fe384e9d5'
