96cc119ff238e60e69a2cb22d348300f8cd8c44e
Stefan Boesen <stefan.boesen@gmail.com>
2012-07-27 11:48:49 -0400

    Added search link helper. Moved search links to last td tag.

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -1,2 +1,11 @@
 module SearchesHelper
+  def search_link_paths(search)
+
+    output = ""
+    output += link_to 'Show', search
+    output += " | "
+    output += link_to 'Edit', edit_search_path(search)
+    output += " | "
+    output += link_to 'Destroy', search, :confirm => 'Are you sure?', :method => :delete
+  end
 end
