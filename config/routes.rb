f6a5076ab2188efe5b8dbf44e3fa1345c3ca24d7
Stefan Boesen <stefan.boesen@gmail.com>
2012-07-27 13:27:01 -0400

    Update default route to index.

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ LdapDartmouth::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'searches#new'
+  root :to => 'searches#index'
 
   mount Sidekiq::Web, at: '/sidekiq'
   # See how all your routes lay out with "rake routes"
