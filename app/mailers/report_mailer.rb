129c916a22989b7a5a239462249f5fdc67a681c0
Stefan Boesen <stefan.boesen@gmail.com>
2012-08-09 10:01:51 -0400

    Pass message to mailer, render message or template if no message.

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,8 +1,13 @@
 class ReportMailer < ActionMailer::Base
   default from: "Dartmouth.Security.Group.Reports@dartmouth.edu"
-  def mail_pdf(email, attachment)
+  def mail_pdf(email, message, attachment)
     attachments['security_report.xls'] = attachment
-    mail(to: email,
-         subject: "Dartmouth Security Group Update (#{Date.today})")
+    mail(to: email, subject: "Dartmouth Security Group Update (#{Date.today})") do |format|
+      if message.nil?
+        format.text { render 'mail_pdf' }
+      else
+        format.text { render :text => message }
+      end
+    end
   end
 end
--- app/workers/mail_worker.rb
+++ app/workers/mail_worker.rb
@@ -8,9 +8,11 @@ class MailWorker
     attachment_url = Rails.application.routes.url_helpers.search_url(search, format: 'xls', :host => 'localhost:3000')
     Rails.logger.info "Sending attachment - #{attachment_url}"
     attachment = open(attachment_url) { |f| f.read }
+    message = search.email_message
+    Rails.logger.info "Sending with message: #{message.to_s}"
     search.user_emails.each do |email|
       email = email.email
-      ReportMailer.mail_pdf(email, attachment).deliver
+      ReportMailer.mail_pdf(email, message, attachment).deliver
     end
   end
 end
