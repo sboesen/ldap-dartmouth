eed766cd57365f62698d79b5119bfe68d3651c22
Stefan Boesen <stefan.boesen@gmail.com>
2012-08-09 10:16:59 -0400

    Production uses dartmouth's mailhub, too.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,11 @@ LdapDartmouth::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "mailhub.dartmouth.edu",
+    :domain               => 'dartmouth.edu'
+  }
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
